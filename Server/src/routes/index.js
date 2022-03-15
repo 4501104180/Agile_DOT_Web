6dd0e9910e6c8f56423ed4bffb98eeb4420b9edf
4501104180 <phucphan785@gmail.com>
2022-03-15 22:08:52 +0700

    Category Manage API

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,9 +1,11 @@
 const accountsRouter = require('./accounts');
 const productsRouter = require('./products');
+const categoriesRouter = require('./categories');
 
 const initialRoutes = (app) => {
     app.use('/api/accounts', accountsRouter);
     app.use('/api/products', productsRouter);
+    app.use('/api/categories', categoriesRouter);
 };
 
 module.exports = initialRoutes;
