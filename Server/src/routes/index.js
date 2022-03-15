e9a2f78ce9f815977cb195719b20d1530b3b72bd
Hương Nguyễn <joenguyentran01@gmail.com>
2022-03-15 21:58:24 +0700

    CRUD and restore API

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,6 +1,8 @@
+const accountsRouter = require('./accounts');
 const productsRouter = require('./products');
 
 const initialRoutes = (app) => {
+    app.use('/api/accounts', accountsRouter);
     app.use('/api/products', productsRouter);
 };
 
